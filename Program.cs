fed8c3e523461111a30fa8aa83c198bb5f60404c
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-01-03 16:30:25 -0500

    Create dotnet new templates

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForEvolve.Conventions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
